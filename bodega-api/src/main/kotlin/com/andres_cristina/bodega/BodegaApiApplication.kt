6fee1463877651f633124b1147653b7623e511c0
AFEscobar2001 <AND.ESCOBARV@DUOCUC.CL>
2025-10-05 18:09:14 -0300

    Initial commit

---
+++ bodega-api/src/main/kotlin/com/andres_cristina/bodega/BodegaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.andres_cristina.bodega
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BodegaApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BodegaApiApplication>(*args)
+}
---
+++ bodega-api/src/test/kotlin/com/andres_cristina/bodega/BodegaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.andres_cristina.bodega
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BodegaApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
